033b4b2236ec4f085d4310b1f885ccbecce1af65
Syeda Zahra <yonasugume@gmail.com>
2025-06-06 15:11:09 +0530

    Commit

---
+++ routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve everything in the directory
+app.use(express.static(__dirname));
+
+// Album route
+app.get('/album', (req, res) => {
+    res.sendFile(path.join(__dirname, 'album.html'));
+});
+
+// Price route
+app.get('/price', (req, res) => {
+    res.sendFile(path.join(__dirname, 'price.html'));
+});
+
+//Form route
+app.get('/form', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact form.html'));
+});
+
+app.listen(30000, () => {
+    console.log('Server running at http://localhost:30000');
+});
